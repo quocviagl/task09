1a7ddd463b31dad29fbe4dc2deaad5f58e6dd99a
Quoc-Vi <quocvi.agl@gmail.com>
2023-01-11 17:29:22 +0700

    PC 40%

---
+++ src/_public/assets/js/main.js
@@ -0,0 +1,10 @@
+$('.c-mainvisual__slider').not('.slick-initialized').slick({
+    infinite: true,
+    autoplay: false,
+    speed: 500,
+    fade: true,
+    cssEase: 'cubic-bezier(0.25, 0.1, 0.25, 1)',
+    dots: false,
+    arrows: false,
+    swipe: false
+  });
