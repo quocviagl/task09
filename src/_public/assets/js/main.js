a582d2b982d4792fc22df86a11f0434e13a5f22e
Quoc-Vi <quocvi.agl@gmail.com>
2023-01-13 17:29:48 +0700

    Recruit section : DONE!

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,7 +1,7 @@
 $('.c-mainvisual__slider').not('.slick-initialized').slick({
     infinite: true,
     autoplay: false,
-    speed: 500,
+    speed: 1000,
     fade: true,
     cssEase: 'cubic-bezier(0.25, 0.1, 0.25, 1)',
     dots: false,
