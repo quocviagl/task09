dfc312c766ded491b02621f495853e02b0df0e1e
Quoc-Vi <quocvi.agl@gmail.com>
2023-01-17 16:58:47 +0700

    100% design PC - without slider change img effect

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,14 +1,33 @@
 $('.c-mainvisual__slider').not('.slick-initialized').slick({
-    infinite: true,
-    autoplay: false,
-    speed: 1000,
-    fade: true,
-    cssEase: 'cubic-bezier(0.25, 0.1, 0.25, 1)',
-    dots: false,
-    arrows: false,
-    swipe: false
+  fade: true,
+  speed: 1100,
+  infinite: true,
+  autoplay: true,
+  cssEase: "linear",
+  arrows: false,
   });
-
+//SCROLL BUTTON
+$(".c-mainvisual__scrolltext--top").on("click", function () {
+    $("html, body").animate(
+      {
+        scrollTop: 800,
+      },
+      1000
+    );
+  });
+//SCROLL ANIMATE
+  $(".c-gnav__item").each(function (i) {
+      $(this).click(function(){
+        var href = $(this).children().attr('href');
+        $("html, body").animate(
+          {
+            scrollTop: $(href).offset().top - 80,
+          },
+          1000
+        );
+      })
+  });
+//SWITCH TAB
 $(".c-table__tab").click(function(){
   $(this).addClass("is-active");
   $(this).siblings().removeClass("is-active");
@@ -23,37 +42,43 @@ $(".c-table__tab").click(function(){
     $('.c-table__content--2').addClass('is-active')
   }
 })
-
+//MENU ACTIVE BORDER
 $(window).scroll(function () {
   let scroll_pos = $(window).scrollTop();
   $(".p-scroll").each(function (i) {
     if (scroll_pos + 500 >= $(this).offset().top) {
       $(".c-gnav__item").siblings().removeClass("is-active");
       $(".c-gnav__item").eq(i).addClass("is-active");
+   
     }
   });
+//FADE IN ANIMATION
+  $(".c-fadein").each(function(i){
+    if(scroll_pos + 800 >= $(this).offset().top){
+      $(this).addClass('is-fadein')
+    }
+  })
 });
-
+//OPEN MESSAGE MODAL
 $( ".c-messages__item" ).click(function() {
       var index = $(".c-messages__item").index( this );
       var modal = "c-messages__modal"
       $('#p-main').addClass("is-hidden")
-      if( (index+1) == 1){
-        $("."+modal+"--"+(index+1)).addClass("is-active")
-      } else if((index+1) == 2){
-        $("."+modal+"--"+(index+1)).addClass("is-active")
-
-      } else{
-        $("."+modal+"--"+(index+1)).addClass("is-active")
-      }
+      $("."+modal+"--"+(index+1)).addClass("is-active")
   });
+//OPEN IMG MODAL
 
-$('.c-messages__modalcontent--bottom').click(function(){
+$( ".c-collection__img" ).click(function() {
+    var index = $(".c-collection__img").index( this )
+    var modal = "c-collection__modal"
+    $('#p-main').addClass("is-hidden")
+    $("."+modal+"--"+(index+1)).addClass("is-active")
+});
+//CLOSE MODAL BUTTON
+$('.c-closebtn').click(function(){
   var modal = $(this).parent().parent().parent()
-  $('.c-messages__list').scrollTop(600)
   if(modal.hasClass('is-active')){
     modal.removeClass('is-active')
     $('#p-main').removeClass("is-hidden")
   }
 })
-
